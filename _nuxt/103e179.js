018d5f5e672437b610fd94a1ad972a5a65b830be
Leon Scherer <scherer-leon@web.de>
2020-12-03 13:57:14 +0100

    dev deploy: Twitter integration

---
+++ _nuxt/103e179.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{236:function(t,n,e){"use strict";e.r(n);var l={},r=e(30),component=Object(r.a)(l,(function(){var t=this.$createElement,n=this._self._c||t;return n("div",[n("nuxt-link",{attrs:{to:"/"}},[this._v("HOME")])],1)}),[],!1,null,null,null);n.default=component.exports}}]);
