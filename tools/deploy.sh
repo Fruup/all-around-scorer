4d03d95d17cb1d540566ed2c9c4ce32bcdc57ab0
yoitsflo19 <florian.michelmann@web.de>
2020-12-03 09:49:06 +0000

    Leon: npm run statt nuxt generate

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -27,7 +27,8 @@ case $CONTINUE in
         cp dist/.git $GIT_TMP
 
         # generate
-        if nuxt generate --fail-on-error
+        if npm run generate --fail-on-error
+        #if nuxt generate --fail-on-error
         then
             # restore .git worktree marker
             mv $GIT_TMP dist/.git
