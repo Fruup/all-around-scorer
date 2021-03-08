9f76fb125d64cd4d75f339603ed94563773e47c9
Leon Scherer <scherer-leon@web.de>
2021-03-08 13:42:24 +0100

    Log tools output to file

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,63 +1,65 @@
-# cd
-cd ..
+{
+    # cd
+    cd ..
 
-# colors
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-NC="\033[0m" # no color
+    # colors
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"
+    NC="\033[0m" # no color
 
-# prompt
-printf "${RED}[IMPORTANT]${NC}: Make sure to preview your site first (preview.sh)!\n\n"
-read -p "Build and deploy website to the public now? (y/n): " CONTINUE
+    # prompt
+    printf "${RED}[IMPORTANT]${NC}: Make sure to preview your site first (preview.sh)!\n\n"
+    read -p "Build and deploy website to the public now? (y/n): " CONTINUE
 
-case $CONTINUE in
-    "y"|"ye"|"yes"|"yea")
+    case $CONTINUE in
+        "y"|"ye"|"yes"|"yea")
 
-        # commit changes to master branch
-        git add content assets/img
-        git commit -m"content update"
-        git push origin master
-
-        # create (new) worktree to track gh-pages branch
-        git worktree remove dist
-        git worktree add dist gh-pages --no-checkout # do not checkout branch, folder will be deleted anyway
-
-        # copy .git marker file (because it will be deleted by nuxt generate)
-        GIT_TMP="~.git.tmp"
-        cp dist/.git $GIT_TMP
-
-        # generate
-        if npm run generate --fail-on-error
-        #if nuxt generate --fail-on-error
-        then
-            # restore .git worktree marker
-            mv $GIT_TMP dist/.git
-
-            # go to dist directory
-            cd dist
-
-            # stage and push dist folder to gh-pages branch
-            git add .
+            # commit changes to master branch
+            git add content assets/img
             git commit -m"content update"
+            git push origin master
 
-            if git push origin gh-pages
+            # create (new) worktree to track gh-pages branch
+            git worktree remove dist
+            git worktree add dist gh-pages --no-checkout # do not checkout branch, folder will be deleted anyway
+
+            # copy .git marker file (because it will be deleted by nuxt generate)
+            GIT_TMP="~.git.tmp"
+            cp dist/.git $GIT_TMP
+
+            # generate
+            if npm run generate --fail-on-error
+            #if nuxt generate --fail-on-error
             then
-                printf "\n\n\n"
-                printf "${GREEN}[INFO]${NC}: Everything worked! The changes to the website are live shortly!\n"
+                # restore .git worktree marker
+                mv $GIT_TMP dist/.git
+
+                # go to dist directory
+                cd dist
+
+                # stage and push dist folder to gh-pages branch
+                git add .
+                git commit -m"content update"
+
+                if git push origin gh-pages
+                then
+                    printf "\n\n\n"
+                    printf "${GREEN}[INFO]${NC}: Everything worked! The changes to the website are live shortly!\n"
+                else
+                    printf "\n\n\n"
+                    printf "${RED}[ERROR]${NC}: Oh no.. Failed to push website to git repository!\n"
+                fi
             else
                 printf "\n\n\n"
-                printf "${RED}[ERROR]${NC}: Oh no.. Failed to push website to git repository!\n"
+                printf "${RED}[ERROR]${NC}: Failed to generate website!\n"
             fi
-        else
+            ;;
+        *)
             printf "\n\n\n"
-            printf "${RED}[ERROR]${NC}: Failed to generate website!\n"
-        fi
-        ;;
-    *)
-        printf "\n\n\n"
-        printf "Ok dann nit.\n"
-        ;;
-esac
+            printf "Ok dann nit.\n"
+            ;;
+    esac
+} | tee "./deploy_log.txt"
 
 # -s: do not echo input
 # -p: prompt message
--- tools/preview.sh
+++ tools/preview.sh
@@ -1,24 +1,29 @@
-# move to project directory (assuming the script was called from within the script folder)
-cd ..
+{
+	# move to project directory (assuming the script was called from within the script folder)
+	cd ..
 
-# colors
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-NC="\033[0m" # no color
+	# colors
+	RED="\033[0;31m"
+	GREEN="\033[0;32m"
+	NC="\033[0m" # no color
 
-# some info
-printf "${GREEN}[INFO]${NC}: ${RED}IMPORTANT${NC}!! Press Ctrl+C to terminate the program when you're done!\n\n\n"
-printf "${GREEN}[INFO]${NC}: This can take a minute..."
-sleep 3
+	# some info
+	printf "${GREEN}[INFO]${NC}: ${RED}IMPORTANT${NC}!! Press Ctrl+C to terminate the program when you're done!\n\n\n"
+	printf "${GREEN}[INFO]${NC}: This can take a minute...\n\n"
+	sleep 3
 
-# pull changes
-git pull
+	# pull changes
+	printf "${GREEN}[INFO]${NC}: Pull changes (git pull)...\n"
+	git pull
 
-# install npm packets
-npm i
+	# install npm packets
+	printf "${GREEN}[INFO]${NC}: Install dependencies (npm i)...\n"
+	npm i
 
-# launch website in default browser
-explorer "http://localhost:3000/"
+	# launch website in default browser
+	explorer "http://localhost:3000/"
 
-# start local dev server
-npm run dev
+	# start local dev server
+	printf "${GREEN}[INFO]${NC}: Launch dev server (npm run dev)...\n"
+	npm run dev
+} | tee "./preview_log.txt"
